83020ba99c20283934fee716407c19f81cec2eba
Nghaiz <153440623+Nghaiz@users.noreply.github.com>
2025-01-21 18:44:37 +0700

    Test logger_config.py

--- clamav_wrapper.py
+++ clamav_wrapper.py
@@ -1,5 +1,7 @@
 import socket
+from logger_config import setup_logger
 
+logger = setup_logger("scan_log.txt")
 
 class ClamavWrapper:
     def __init__(self, host: str, port: int):
@@ -8,23 +10,36 @@ class ClamavWrapper:
 
         try:
             self.sock = socket.create_connection((self.host, self.port))
+            logger.info(f"Connected to clamd server at {self.host}:{self.port}")
         except Exception as e:
-            raise ConnectionError('cannot connect to clamd. is the clamd server running?')
+            logger.error("Cannot connect to clamd server. Is the clamd server running?")
+            logger.error(f"Error details: {e}")
+            raise ConnectionError("Cannot connect to clamd. Is the clamd server running?")
 
     def scan_file(self, file_path: str):
-        file = open(file_path, 'rb')
-        file_content = file.read()
-        file.close()
+        try:
+            with open(file_path, 'rb') as file:
+                file_content = file.read()
+            logger.info(f"Scanning file: {file_path}")
 
-        self.sock.sendall(b'zINSTREAM\0')
+            self.sock.sendall(b'zINSTREAM\0')
 
-        # send content to clamd each chunk size
-        chunk_size = 8192
-        for i in range(0, len(file_content), chunk_size):
-            chunk = file_content[i:i + chunk_size]
-            self.sock.sendall(len(chunk).to_bytes(4, byteorder='big') + chunk)
+            # Gửi nội dung file theo từng chunk
+            chunk_size = 8192
+            for i in range(0, len(file_content), chunk_size):
+                chunk = file_content[i:i + chunk_size]
+                self.sock.sendall(len(chunk).to_bytes(4, byteorder='big') + chunk)
 
-        self.sock.sendall(b'\0\0\0\0')
+            self.sock.sendall(b'\0\0\0\0')
 
-        response = self.sock.recv(1024)
-        return response.decode('utf-8')[8:]
+            response = self.sock.recv(1024)
+            result = response.decode('utf-8')[8:]
+
+            logger.info(f"Scan result for {file_path}: {result}")
+            return result
+        except FileNotFoundError:
+            logger.warning(f"File not found: {file_path}")
+            return "File not found"
+        except Exception as e:
+            logger.error(f"Error while scanning file {file_path}: {e}")
+            raise
